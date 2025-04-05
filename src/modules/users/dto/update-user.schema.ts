dc6c65ab9dd260530f2f0c6691de9ce4d29e671d
Roma <zixrom@gmail.com>
2025-04-05 22:23:12 +0000

    chore(user): update update-user-schema

--- src/modules/users/dto/update-user.schema.ts
+++ src/modules/users/dto/update-user.schema.ts
@@ -5,7 +5,7 @@ export const updateUserSchema = z
     firstName: z.string().min(1).max(100).optional(),
     lastName: z.string().min(1).max(100).optional(),
     phone: z.string().max(100).optional(),
-    birthDate: z.string().max(100).datetime().optional(),
+    birthDate: z.coerce.date().optional(),
     email: z.string().email().optional(),
     about: z.string().max(1000).optional(),
   })
